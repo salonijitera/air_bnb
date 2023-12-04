b2f2fdb0c9a27bbf0595eb62dba32ee8293d34e3
Kass (Jitera) <developers@jitera.com>
2023-12-04 13:00:55 +0000

    feat[business-logic]: implement 'User registration' Changes * '/app/controllers/api/registrations_controller.rb': + Create a new method `create` in the RegistrationsController. This method will handle the user registration process..+ The `create` method should receive the registration request with the user's username, password, and email as parameters..+ Use the `valid?` method from the User model to validate t...

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -7,7 +7,7 @@ class Api::RegistrationsController < ApplicationController
         @user.password = BCrypt::Password.create(@user.password)
         @user.save
         ApplicationMailer.confirmation_email(@user).deliver_now
-        render json: { user: @user, confirmation_status: 'Email sent, please confirm your email address' }, status: 201
+        render json: { user: @user.slice(:id, :username, :email), confirmation_status: 'Email sent, please confirm your email address' }, status: 201
       else
         render json: { error: 'Email already registered' }, status: 400
       end
@@ -17,6 +17,6 @@ class Api::RegistrationsController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
