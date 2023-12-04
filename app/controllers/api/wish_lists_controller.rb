4977c9e7e71dd30bf220f393f176ddb812bd0e34
Belia (Jitera) <developers@jitera.com>
2023-12-04 12:34:26 +0000

    feat[business-logic]: implement 'Add property to wish list' Changes * '/app/controllers/api/wish_lists_controller.rb': + Create a new method `add_property` in the WishListsController. This method will handle the logic of adding a property to a wish list..+ The `add_property` method should take in two parameters: `wish_list_id` and `property_id`..+ First, validate the `wish_list_id` and `property_id` by checking if t...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -15,4 +15,26 @@ class Api::WishListsController < ApplicationController
   rescue => e
     render json: { error: e.message }, status: 500
   end
+  def add_property
+    wish_list_id = params[:wish_list_id]
+    property_id = params[:property_id]
+    # Validate wish_list_id and property_id
+    unless WishList.exists?(wish_list_id) && Property.exists?(property_id)
+      return render json: { error: 'Invalid wish list or property id' }, status: 404
+    end
+    # Check if the property is already in the wish list
+    if WishListItem.exists?(wish_list_id: wish_list_id, property_id: property_id)
+      return render json: { error: 'Property is already in the wish list' }, status: 400
+    end
+    # Add the property to the wish list
+    wish_list_item = WishListItem.new(wish_list_id: wish_list_id, property_id: property_id)
+    if wish_list_item.save
+      # Send confirmation to the user
+      render json: { status: 200, message: 'Property successfully added to the wish list', wish_list: WishList.find(wish_list_id).as_json(include: :properties) }
+    else
+      render json: { error: 'Failed to add property to the wish list' }, status: 500
+    end
+  rescue => e
+    render json: { error: e.message }, status: 500
+  end
 end
