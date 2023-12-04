800aa5b6d1f51a9a10003be490fc37c324bf6946
Adrea (Jitera) <developers@jitera.com>
2023-12-04 12:52:48 +0000

    feat[business-logic]: implement 'Register user' Changes * '/app/controllers/api/sessions_controller.rb': + Create a new method `register` in this controller. This method will handle the registration process..+ The `register` method should receive the user's name, email, and password as parameters..+ Use the `User` model to validate the input data. Ensure all required fields are filled and the email for...

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -19,6 +19,22 @@ class Api::SessionsController < ApplicationController
       render json: ["No currently logged in user"], status: 401
     end
   end
+  def register
+    @user = User.new(user_params)
+    if @user.valid?
+      existing_user = User.find_by_email(@user.email)
+      if existing_user.nil?
+        @user.password = BCrypt::Password.create(@user.password)
+        @user.save
+        ApplicationMailer.confirmation_email(@user).deliver_now
+        render json: { user: @user, confirmation_status: 'Email sent, please confirm your email address' }, status: 201
+      else
+        render json: { error: 'Email already registered' }, status: 400
+      end
+    else
+      render json: { error: 'Invalid user data' }, status: 400
+    end
+  end
   def update
     @user = User.find(params[:id])
     if @user
@@ -32,5 +48,9 @@ class Api::SessionsController < ApplicationController
     else
       render json: { message: 'User not found' }, status: 404
     end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
