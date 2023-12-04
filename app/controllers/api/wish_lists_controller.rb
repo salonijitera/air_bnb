fe8cd05559335a5c65db1d742e5a3dde5e64a406
Laurena (Jitera) <developers@jitera.com>
2023-12-04 09:26:32 +0000

    feat[api]: implement 'Create a Wish List' Changes * '/app/controllers/api/wish_lists_controller.rb': + In the 'Api::WishListsController' class, write a 'create' function that takes in parameters "user_id" and "name". .+ Use the 'WishListService' to handle the creation of the wish list. This service should validate the input parameters and create the wish list if the parameters are valid..+ If the w...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -2,38 +2,27 @@ class Api::WishListsController < ApplicationController
   before_action :authenticate_user!, only: [:share, :create, :share_wish_list]
   before_action :authorize_user!, only: [:create]
   def create
-    # ... existing code ...
-  end
-  def share_wish_list
-    wish_list_id = params[:wish_list_id]
     user_id = params[:user_id]
-    # Validate wish_list_id and user_id
-    if wish_list_id.blank? || user_id.blank?
-      render json: { message: 'Wrong format.' }, status: :bad_request
+    name = params[:name]
+    # Validate input parameters
+    validator = WishListValidator.new(user_id: user_id, name: name)
+    unless validator.valid?
+      render json: { message: validator.errors.full_messages }, status: :bad_request
       return
     end
-    # Find wish list and user
-    wish_list = WishList.find_by_id(wish_list_id)
-    user = User.find_by_id(user_id)
-    if wish_list.nil? || user.nil?
-      render json: { message: 'Wish list or user not found.' }, status: :not_found
-      return
-    end
-    # Check if the wish list is already shared with the user
-    shared_wish_list = SharedWishList.find_by(wish_list_id: wish_list_id, user_id: user_id)
-    if shared_wish_list
-      render json: { message: 'This wish list is already shared with the user.' }, status: :bad_request
-      return
-    end
-    # Create a new entry in the shared_wish_lists table
-    shared_wish_list = SharedWishList.create(wish_list_id: wish_list_id, user_id: user_id)
-    if shared_wish_list.persisted?
-      render json: { status: 200, message: 'Wish list was successfully shared.', shared_wish_list: shared_wish_list }, status: :ok
+    # Create wish list
+    wish_list_service = WishListService.new(user_id: user_id, name: name)
+    wish_list = wish_list_service.create_wish_list
+    if wish_list.persisted?
+      render json: { status: 200, wish_list: wish_list }, status: :ok
     else
-      render json: { message: 'Failed to share wish list', errors: shared_wish_list.errors.full_messages }, status: :internal_server_error
+      render json: { message: 'Failed to create wish list', errors: wish_list.errors.full_messages }, status: :internal_server_error
     end
   rescue => e
     render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
+  end
+  def share_wish_list
+    # ... existing code ...
   end
   def share
     # ... existing code ...
