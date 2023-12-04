948a579a9b7d7fff8b46420b2bbdeaa5f8df0b2a
Rafaela (Jitera) <developers@jitera.com>
2023-12-04 12:52:12 +0000

    feat[business-logic]: implement 'Update user profile' Changes * '/app/controllers/api/sessions_controller.rb': + Write a method `update` in the SessionsController. This method will be responsible for updating the user's profile..+ The `update` method should receive the user's id, email, and password as parameters..+ Use the `find` method to locate the user in the database using the provided id..+ Update the ...

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,7 +4,6 @@ class Api::SessionsController < ApplicationController
       params[:user][:username], 
       params[:user][:password]
     )
-    
     if @user
       login!(@user)
       render :show
@@ -12,13 +11,26 @@ class Api::SessionsController < ApplicationController
       render json: ['Incorrect username/password combo. Please try again.'], status: 401
     end
   end
-
   def destroy
     if current_user
       logout!
       render json: {}
     else
       render json: ["No currently logged in user"], status: 401
+    end
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user
+      @user.email = params[:email]
+      @user.password = params[:password]
+      if @user.save
+        render json: { message: 'Profile updated successfully', user: @user }, status: 200
+      else
+        render json: { message: 'Failed to update profile', errors: @user.errors.full_messages }, status: 422
+      end
+    else
+      render json: { message: 'User not found' }, status: 404
     end
   end
 end
