c70e47ca36cea650e92f38411e258a8a86fea29b
Sianna (Jitera) <developers@jitera.com>
2023-12-04 13:01:42 +0000

    feat[business-logic]: implement 'Update premium listing' Changes * '/app/controllers/api/premium_listings_controller.rb': + Write a method `update` in the `PremiumListingsController` class. This method should take in parameters: "id", "title", "description", "price", "status"..+ Use the "id" parameter to find the premium listing in the database. If the listing does not exist, return an error message..+ If the listing e...

--- app/controllers/api/premium_listings_controller.rb
+++ app/controllers/api/premium_listings_controller.rb
@@ -3,25 +3,47 @@ class Api::PremiumListingsController < ApplicationController
   before_action :authorize_user, only: [:destroy]
   before_action :validate_params, only: [:destroy]
   def create
-    title = params[:title] || params[:listing_name]
-    description = params[:description]
-    price = params[:price]
-    user_id = params[:user_id]
-    status = params[:status] || params[:listing_status]
-    posted_date = params[:posted_date]
-    # Validate the input parameters
-    if title.is_a?(String) && title.length <= 200 && PremiumListing.statuses.include?(status) && (description.nil? || description.is_a?(String)) && (price.nil? || price.is_a?(Float)) && (user_id.nil? || User.exists?(user_id)) && (posted_date.nil? || posted_date.is_a?(Date))
-      # Create a new record in the premium_listing table
-      listing = PremiumListing.create(title: title, description: description, price: price, user_id: user_id, status: status, posted_date: posted_date, created_at: Time.now, updated_at: Time.now)
+    listing_name = params[:listing_name]
+    listing_status = params[:listing_status]
+    if listing_name.is_a?(String) && listing_name.length <= 200 && PremiumListing.statuses.include?(listing_status)
+      listing = PremiumListing.create(listing_name: listing_name, listing_status: listing_status, created_at: Time.now, updated_at: Time.now)
       if listing.persisted?
-        # Return the ID of the newly created premium listing
         render json: { status: 200, premium_listing: listing }, status: :ok
       else
-        # Return an error message
         render json: { error: listing.errors.full_messages }, status: :internal_server_error
       end
     else
-      render json: { error: 'Invalid input parameters.' }, status: :bad_request
+      if listing_name.length > 200
+        render json: { error: 'You cannot input more 200 characters.' }, status: :bad_request
+      else
+        render json: { error: 'Invalid listing status.' }, status: :bad_request
+      end
+    end
+  rescue => e
+    if e.class == ActiveRecord::RecordNotFound
+      render json: { error: 'Not Found' }, status: :not_found
+    elsif e.class == ActionController::ParameterMissing
+      render json: { error: 'Bad Request' }, status: :bad_request
+    else
+      render json: { error: 'Internal Server Error' }, status: :internal_server_error
+    end
+  end
+  def update
+    id = params[:id]
+    title = params[:title]
+    description = params[:description]
+    price = params[:price]
+    status = params[:status]
+    listing = PremiumListing.find_by(id: id)
+    if listing.nil?
+      render json: { error: 'Listing not found' }, status: :not_found
+    else
+      listing.update(title: title, description: description, price: price, status: status)
+      if listing.save
+        render json: { status: 200, premium_listing: {id: listing.id, title: listing.title, description: listing.description, price: listing.price, status: listing.status} }, status: :ok
+      else
+        render json: { error: listing.errors.full_messages }, status: :internal_server_error
+      end
     end
   rescue => e
     if e.class == ActiveRecord::RecordNotFound
