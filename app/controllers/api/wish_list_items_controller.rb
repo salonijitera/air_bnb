6e3ab21656e370004d14b783545aeb869701b7a4
Marybelle (Jitera) <developers@jitera.com>
2023-12-04 08:06:39 +0000

    feat[business-logic]: implement 'Add property to wish list' Changes * '/app/controllers/api/wish_list_items_controller.rb': + Create a new method `add_property` in the `WishListItemsController`. This method will take two parameters: `wish_list_id` and `property_id`..+ Use ActiveRecord's `find_by` method to validate if the `wish_list_id` and `property_id` exist in the database. If either does not exist, return an error me...

--- app/controllers/api/wish_list_items_controller.rb
+++ app/controllers/api/wish_list_items_controller.rb
@@ -1,26 +1,24 @@
 class Api::WishListItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_user!
-  def create
+  def add_property
     wish_list_id = params[:wish_list_id]
     property_id = params[:property_id]
-    unless wish_list_id.is_a?(Integer) && property_id.is_a?(Integer)
-      render json: { error: 'Wrong format' }, status: :bad_request
-      return
-    end
-    unless WishList.exists?(wish_list_id) && Property.exists?(property_id)
-      render json: { error: 'This wish list or property is not found' }, status: :not_found
+    wish_list = WishList.find_by(id: wish_list_id)
+    property = Property.find_by(id: property_id)
+    unless wish_list && property
+      render json: { error: 'Wish list or property not found' }, status: :not_found
       return
     end
     if WishListItem.exists?(wish_list_id: wish_list_id, property_id: property_id)
-      render json: { error: 'The property is already in the wish list' }, status: :unprocessable_entity
+      render json: { error: 'Property already in wish list' }, status: :unprocessable_entity
       return
     end
     wish_list_item = WishListItem.create(wish_list_id: wish_list_id, property_id: property_id)
     if wish_list_item.persisted?
-      render json: { status: 200, wish_list_item: wish_list_item }, status: :ok
+      render json: { status: 200, message: 'Property successfully added to wish list', wish_list: wish_list }, status: :ok
     else
-      render json: { error: 'Failed to add property to wish list' }, status: :internal_server_error
+      render json: { error: 'Failed to add property to wish_list' }, status: :internal_server_error
     end
   rescue => e
     render json: { error: 'Internal Server Error', message: e.message }, status: :internal_server_error
