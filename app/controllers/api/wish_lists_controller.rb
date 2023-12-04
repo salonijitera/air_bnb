6b192a17fe17c3560e7ea50dc661f339f9351281
Marget (Jitera) <developers@jitera.com>
2023-12-04 07:05:31 +0000

    feat[business-logic]: implement 'Share wish list' Changes * '/app/controllers/api/wish_lists_controller.rb': + Create a new method `share_wish_list` in the WishListsController. This method will handle the logic of sharing a wish list..+ The `share_wish_list` method should take in two parameters: `wish_list_id` and `user_id`. These parameters can be obtained from the request body..+ Use ActiveRecord's `find...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -12,4 +12,26 @@ class Api::WishListsController < ApplicationController
       render json: { message: 'User not found' }, status: :not_found
     end
   end
+  def share_wish_list
+    wish_list_id = params[:wish_list_id]
+    user_id = params[:user_id]
+    wish_list = WishList.find_by(id: wish_list_id)
+    user = User.find_by(id: user_id)
+    if wish_list.nil? || user.nil?
+      render json: { error: 'Wish list or user not found' }, status: :not_found
+      return
+    end
+    shared_link = generate_shared_link(wish_list_id)
+    send_shared_link_to_user(user, shared_link)
+    render json: { message: 'Wish list shared successfully' }, status: :ok
+  end
+  private
+  def generate_shared_link(wish_list_id)
+    "https://yourwebsite.com/wish_lists/#{wish_list_id}"
+  end
+  def send_shared_link_to_user(user, shared_link)
+    # This is a placeholder and should be replaced with your actual implementation.
+    # For example, you could send an email or a notification within your application.
+    # UserMailer.share_wish_list(user, shared_link).deliver_now
+  end
 end
