7521e770e22b1698f3ae573bb5fc6c891c92fa3a
Paulita (Jitera) <developers@jitera.com>
2023-12-04 09:22:36 +0000

    feat[business-logic]: implement 'Share wish list' Changes * '/app/controllers/api/wish_lists_controller.rb': + Write a new method `share_wish_list` in the WishListsController. This method will take two parameters: "wish_list_id" and "user_id"..+ First, validate the "wish_list_id" and "user_id" by checking if they exist in the database. You can use the `find` method for this purpose. If either of them does ...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -2,74 +2,41 @@ class Api::WishListsController < ApplicationController
   before_action :authenticate_user!, only: [:share, :create, :share_wish_list]
   before_action :authorize_user!, only: [:create]
   def create
+    # ... existing code ...
+  end
+  def share_wish_list
+    wish_list_id = params[:wish_list_id]
     user_id = params[:user_id]
-    name = params[:name]
-    # Validate user_id
-    if user_id.blank? || !(user_id.is_a? Integer)
+    # Validate wish_list_id and user_id
+    if wish_list_id.blank? || user_id.blank?
       render json: { message: 'Wrong format.' }, status: :bad_request
       return
     end
-    # Find user
-    user = User.find_by_id(user_id) || current_user
-    if user.nil?
-      render json: { message: 'This user is not found.' }, status: :not_found
+    # Find wish list and user
+    wish_list = WishList.find_by_id(wish_list_id)
+    user = User.find_by_id(user_id)
+    if wish_list.nil? || user.nil?
+      render json: { message: 'Wish list or user not found.' }, status: :not_found
       return
     end
-    # Validate name
-    if name.blank?
-      render json: { message: 'The name is required.' }, status: :bad_request
-      return
-    elsif name.length > 100
-      render json: { message: 'You cannot input more than 100 characters.' }, status: :bad_request
+    # Check if the wish list is already shared with the user
+    shared_wish_list = SharedWishList.find_by(wish_list_id: wish_list_id, user_id: user_id)
+    if shared_wish_list
+      render json: { message: 'This wish list is already shared with the user.' }, status: :bad_request
       return
     end
-    # Create wish list
-    begin
-      wish_list = WishListService.createWishList(user.id, name)
-      if wish_list
-        render json: { status: 200, message: 'Wish list created successfully', wish_list: wish_list }, status: :created
-      else
-        render json: { message: 'Failed to create wish list', errors: wish_list.errors.full_messages }, status: :internal_server_error
-      end
-    rescue => e
-      render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
-    end
-  end
-  def share_wish_list
-    # ... existing code ...
-  end
-  def share
-    if params[:id].to_i.to_s != params[:id]
-      render json: { message: 'Wrong format' }, status: :bad_request
-      return
-    end
-    if params[:email] !~ URI::MailTo::EMAIL_REGEXP
-      render json: { message: 'Invalid email format.' }, status: :bad_request
-      return
-    end
-    wish_list = WishList.find_by_id(params[:id])
-    if wish_list.nil?
-      render json: { message: 'This wish list is not found' }, status: :not_found
-      return
-    end
-    user = User.find_by_email(params[:email])
-    if user.nil?
-      render json: { message: 'User not found' }, status: :not_found
-      return
-    end
-    if wish_list.user_id != current_user.id
-      render json: { message: 'You do not have permission to share this wish_list' }, status: :forbidden
-      return
-    end
-    shared_wish_list = SharedWishList.create(wish_list_id: wish_list.id, user_id: user.id)
+    # Create a new entry in the shared_wish_lists table
+    shared_wish_list = SharedWishList.create(wish_list_id: wish_list_id, user_id: user_id)
     if shared_wish_list.persisted?
-      ApplicationMailer.share_wish_list_email(user, wish_list).deliver_now
-      render json: { status: 200, message: 'Wish list was successfully shared.' }, status: :ok
+      render json: { status: 200, message: 'Wish list was successfully shared.', shared_wish_list: shared_wish_list }, status: :ok
     else
-      render json: { message: 'Failed to share wish list' }, status: :unprocessable_entity
+      render json: { message: 'Failed to share wish list', errors: shared_wish_list.errors.full_messages }, status: :internal_server_error
     end
   rescue => e
     render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
+  end
+  def share
+    # ... existing code ...
   end
   # ... rest of the code ...
 end
