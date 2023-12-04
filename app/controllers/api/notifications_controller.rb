d1cf3eebc336371dd54b7abfd4eeb4cf862b990d
Marylinda (Jitera) <developers@jitera.com>
2023-12-04 12:42:14 +0000

    feat[api]: implement 'Receive Notifications' Changes * '/app/controllers/api/notifications_controller.rb': + Create a new method `get_notifications` in the `NotificationsController` class..+ This method will handle the GET request to the `/api/notifications` endpoint..+ It will receive the `user_id` as a parameter from the request..+ Use the `NotificationService` to fetch the notifications for the given ...

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -1,15 +1,15 @@
 class Api::NotificationsController < ApplicationController
-  before_action :authenticate_user, only: [:index]
-  before_action :authorize_user, only: [:index]
+  before_action :authenticate_user, only: [:index, :get_notifications]
+  before_action :authorize_user, only: [:index, :get_notifications]
   def index
     begin
       user_id = params[:user_id].to_i
       if user_id.nil? || !user_id.is_a?(Integer)
         render json: { error: 'Wrong format' }, status: :bad_request
       else
-        user = User.find(user_id)
+        user = User.find_by(id: user_id)
         if user.nil?
-          render json: { error: 'User not found' }, status: :not_found
+          render json: { error: 'This user is not found' }, status: :not_found
         else
           notifications = Notification.where(user_id: user_id)
           if notifications.empty?
@@ -18,8 +18,25 @@ class Api::NotificationsController < ApplicationController
             notifications.each do |notification|
               notification.update(status: 'read')
             end
-            render json: { status: 200, notifications: notifications.as_json(only: [:id, :message, :status]) }, status: :ok
+            render json: { status: 200, notifications: notifications.as_json(only: [:id, :message, :status, :user_id]) }, status: :ok
           end
+        end
+      end
+    rescue => e
+      render json: { error: 'An unexpected error occurred' }, status: :internal_server_error
+    end
+  end
+  def get_notifications
+    begin
+      user_id = params[:user_id].to_i
+      if user_id.nil? || !user_id.is_a?(Integer)
+        render json: { error: 'Wrong format' }, status: :bad_request
+      else
+        notifications = NotificationService.get_notifications(user_id)
+        if notifications.empty?
+          render json: { error: 'No notifications found for this user' }, status: :not_found
+        else
+          render json: { status: 200, notifications: notifications.as_json(only: [:id, :message, :status, :user_id]) }, status: :ok
         end
       end
     rescue => e
