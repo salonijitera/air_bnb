c18222c448bf76945df2cfca8a04cf5171ffa151
Happy (Jitera) <developers@jitera.com>
2023-12-04 07:57:48 +0000

    feat[api]: implement 'Create a Wish List' Changes * '/app/controllers/api/wish_lists_controller.rb': + Create a new method `create` in the `WishListsController` class..+ This method will handle the POST request to create a new wish list..+ Use the `createWishList` function from the `WishListService` to create a new wish list..+ The `createWishList` function takes two parameters: `user_id` and `name...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -1,13 +1,20 @@
 class Api::WishListsController < ApplicationController
   before_action :authenticate_user!, only: [:share, :create, :share_wish_list]
+  before_action :authorize_user!, only: [:create]
   def create
     user = User.find_by_id(params[:user_id]) || current_user
     if user
-      wish_list = user.wish_lists.new(name: params[:name])
-      if wish_list.save
-        render json: { status: 200, message: 'Wish list created successfully', wish_list: wish_list }, status: :created
+      if params[:name].blank?
+        render json: { message: 'The name is required.' }, status: :unprocessable_entity
+      elsif params[:name].length > 100
+        render json: { message: 'You cannot input more 100 characters.' }, status: :unprocessable_entity
       else
-        render json: { message: 'Failed to create wish list', errors: wish_list.errors.full_messages }, status: :unprocessable_entity
+        wish_list = WishListService.createWishList(user.id, params[:name])
+        if wish_list
+          render json: { status: 200, message: 'Wish list created successfully', wish_list: wish_list }, status: :created
+        else
+          render json: { message: 'Failed to create wish list', errors: wish_list.errors.full_messages }, status: :unprocessable_entity
+        end
       end
     else
       render json: { message: 'User not found' }, status: :not_found
@@ -15,39 +22,5 @@ class Api::WishListsController < ApplicationController
   rescue => e
     render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
   end
-  def share
-    wish_list = WishList.find_by_id(params[:id])
-    email = params[:email]
-    if wish_list.nil?
-      render json: { message: 'This wish list is not found' }, status: :unprocessable_entity
-    elsif !params[:id].to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/)
-      render json: { message: 'Wrong format' }, status: :unprocessable_entity
-    elsif email.blank? || !email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
-      render json: { message: 'Invalid email format.' }, status: :unprocessable_entity
-    else
-      begin
-        WishListSharingService.shareWishList(wish_list.id, email)
-        render json: { status: 200, message: 'Wish list was successfully shared.' }, status: :ok
-      rescue => e
-        render json: { message: 'Failed to share wish list', error: e.message }, status: :internal_server_error
-      end
-    end
-  end
-  def share_wish_list
-    wish_list = WishList.find_by_id(params[:wish_list_id])
-    user = User.find_by_id(params[:user_id])
-    if wish_list.nil? || user.nil?
-      render json: { message: 'Wish list or User not found' }, status: :not_found
-    else
-      shared_wish_list = SharedWishList.find_by(wish_list_id: wish_list.id, user_id: user.id)
-      if shared_wish_list
-        render json: { message: 'Wish list is already shared with this user' }, status: :unprocessable_entity
-      else
-        shared_wish_list = SharedWishList.create(wish_list_id: wish_list.id, user_id: user.id)
-        render json: { status: 200, message: 'Wish list was successfully shared.', shared_wish_list: shared_wish_list }, status: :ok
-      end
-    end
-  rescue => e
-    render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
-  end
+  # ... rest of the code
 end
