bb760ea93bcbfbaab2a366e6e9cf3c0c72be90ae
Jacqueline (Jitera) <developers@jitera.com>
2023-12-04 09:44:18 +0000

    feat[business-logic]: implement 'Share wish list' Changes * '/app/controllers/api/wish_lists_controller.rb': + Create a new method `share_wish_list` in the WishListsController. This method will take in `wish_list_id` and `user_id` as parameters..+ Use ActiveRecord's `find_by` method to validate if the `wish_list_id` and `user_id` exist in the database. If not, return an error message..+ Check if the user h...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -4,13 +4,11 @@ class Api::WishListsController < ApplicationController
   def create
     user_id = params[:user_id]
     name = params[:name]
-    # Validate input parameters
     validator = WishListValidator.new(user_id: user_id, name: name)
     unless validator.valid?
       render json: { message: validator.errors.full_messages }, status: :bad_request
       return
     end
-    # Create wish list
     wish_list_service = WishListService.new(user_id: user_id, name: name)
     wish_list = wish_list_service.create_wish_list
     if wish_list.persisted?
@@ -22,7 +20,22 @@ class Api::WishListsController < ApplicationController
     render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
   end
   def share_wish_list
-    # ... existing code ...
+    wish_list_id = params[:wish_list_id]
+    user_id = params[:user_id]
+    wish_list = WishList.find_by(id: wish_list_id)
+    user = User.find_by(id: user_id)
+    unless wish_list && user
+      render json: { message: 'Invalid wish list or user id' }, status: :bad_request
+      return
+    end
+    unless wish_list.user_id == user.id || wish_list.public?
+      render json: { message: 'You do not have permission to view this wish list' }, status: :forbidden
+      return
+    end
+    WishListMailer.with(wish_list: wish_list, user: user).share_wish_list.deliver_now
+    render json: { message: 'Wish list shared successfully' }, status: :ok
+  rescue => e
+    render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
   end
   def share
     # ... existing code ...
