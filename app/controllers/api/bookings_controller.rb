4bb0388ea3177a095a6ef604203d7ee9fecec3b2
Ynez (Jitera) <developers@jitera.com>
2023-12-04 07:05:36 +0000

    feat[business-logic]: implement 'Book property' Changes * '/app/controllers/api/bookings_controller.rb': + Create a new method `create` in the `BookingsController` class. This method will handle the booking request from the user..+ The `create` method should take in `property_id` and `user_id` as parameters. These parameters can be obtained from the request body..+ Use ActiveRecord's `find` method to v...

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -5,41 +5,41 @@ class Api::BookingsController < ApplicationController
                   .order(start_date: :asc)
     render :index
   end
-
   def show
     @booking = Booking.includes(:user).includes(:listing).find_by(id: params[:id])
     render :show
   end
-
   def create
-    @booking = Booking.new(booking_params)
-
-    if @booking.save!
-      render :show
+    property = Property.find_by(id: params[:property_id])
+    user = User.find_by(id: params[:user_id])
+    if property.nil? || user.nil?
+      render json: { error: 'Invalid property or user id' }, status: 404
+    elsif property.availability == false
+      render json: { error: 'Property is not available' }, status: 400
     else
-      render json: ['Booking is invalid.'], status: 401      
+      property.update(availability: false)
+      @booking = Booking.new(user_id: user.id, listing_id: property.id)
+      if @booking.save
+        render json: @booking, status: 201
+      else
+        render json: { error: 'Failed to create booking' }, status: 500
+      end
     end
   end
-
   def destroy
     @booking = Booking.find_by(id: params[:id])
     @booking.destroy!
     render json: ["Booking successfully removed."]
   end
-
   def update
-    @booking = Booking.find_by(params[:id])
-    @booking.update_attributes(booking_params)
-
-    if @booking.save!
+    @booking = Booking.find_by(id: params[:id])
+    if @booking.update(booking_params)
       render :show
     else
       render json: ['Unable to update booking'], status: 401
     end
   end
-
   private
-
   def booking_params
     params.require(:booking).permit(:id, :start_date, :end_date, :num_guests, :listing_id, :user_id)
   end
