88a2d7c684517dd48930ad5b4d6a35397d6c297d
Sondra (Jitera) <developers@jitera.com>
2023-12-04 12:30:27 +0000

    feat[database]: delete tables 'wish_list_items' and update table 'wish_lists' Changes * '/db/migrate/20220404162314_create_wish_lists.rb': created * '/app/models/wish_list.rb': created + '/app/controllers/api/wish_list_items_controller.rb': deleted + '/app/controllers/api/wish_lists_controller.rb': deleted + '/app/mailers/wish_list_mailer.rb': deleted + '/app/models/wish_list.rb': deleted + '/app/services/wish_list_service.rb': deleted + '/app/validators/wish_list_validator.rb': deleted + '/app/views/api/wish_lists/_wish_list.json.jbuilder': deleted + '/app/views/api/wish_lists/create.json.jbuilder': deleted

--- app/mailers/wish_list_mailer.rb
+++
@@ -1,9 +0,0 @@
-class WishListMailer < ApplicationMailer
-  default from: 'notifications@example.com'
-  def wish_list_created(wish_list)
-    @wish_list = wish_list
-    @user = @wish_list.user
-    @url  = wish_list_url(@wish_list)
-    mail(to: @user.email, subject: 'Wish List Created', body: "Your wish list #{@wish_list.name} has been created successfully.")
-  end
-end
--- app/views/api/wish_lists/create.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.status 200
-json.wish_list do
-  json.extract! @wish_list, :id, :name
-end
