8aaa9cb73ffa9c12c4786d6944b5a0b697e77ae9
Gunilla (Jitera) <developers@jitera.com>
2023-12-04 09:58:38 +0000

    feat[api]: implement 'Book Property' Changes * '/app/models/booking.rb': + Add validations for the `date_from` and `date_to` attributes in the `Booking` model. Use the `validates` method with the `:date` option to ensure that these attributes are valid dates..+ Add a custom validation method to check that `date_from` is not after `date_to`. If it is, add an error to the ...

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,6 +8,7 @@ class Booking < ApplicationRecord
   validates :user_id, :listing_id, presence: true, numericality: { only_integer: true }
   validates :start_date, :end_date, presence: true
   validate :date_from_must_be_before_date_to
+  validates_date :start_date, :end_date
   def self.book_property(user_id, property_id, date_from, date_to)
     user = User.find_by(id: user_id)
     return { error: 'This user is not found' } unless user
