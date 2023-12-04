122974737c4d99fc9cc679c6d359fb63857160db
Brandais (Jitera) <developers@jitera.com>
2023-12-04 09:44:52 +0000

    feat[business-logic]: implement 'Book property' Changes * '/app/models/booking.rb': + Define a new method 'book_property' in the Booking model. This method will handle the business logic of booking a property..+ The 'book_property' method should take in 'user_id' and 'property_id' as parameters..+ Use ActiveRecord's 'find' method to validate the existence of the user and the proper... * '/app/models/listing.rb': + Define a new method 'update_status' in the Listing model. This method will handle the updating of the property status..+ The 'update_status' method should take in 'property_id' as a parameter..+ Use ActiveRecord's 'find' method to validate the existence of the property in the database. If the prop...

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,22 +4,28 @@
 #
 #  id         :bigint           not null, primary key
 #  start_date :date
-#  end_date   :date
+#  end_date   date
 #  num_guests :integer
 #  listing_id :integer
 #  user_id    :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 class Booking < ApplicationRecord
   belongs_to :listing,
     foreign_key: :listing_id,
     class_name: 'Listing'
-  
   belongs_to :user,
     foreign_key: :user_id,
     class_name: 'User'
-
-    
+  def self.book_property(user_id, property_id)
+    user = User.find_by(id: user_id)
+    return { error: 'User not found' } unless user
+    property = Listing.find_by(id: property_id)
+    return { error: 'Property not found' } unless property
+    return { error: 'Property is not available' } unless property.availability
+    booking = self.create(user_id: user_id, listing_id: property_id)
+    property.update(availability: false)
+    { success: 'Booking successful', booking: booking }
+  end
 end
