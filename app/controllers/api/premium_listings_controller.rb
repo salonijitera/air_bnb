f729df20fed784b76a36bb964d3d0ee8367bb510
Britta (Jitera) <developers@jitera.com>
2023-12-04 14:20:07 +0000

    feat[api]: implement 'Create Premium Listing for VIP Users' Changes * '/app/controllers/api/premium_listings_controller.rb': + Create a new method `create` in the `PremiumListingsController` class. This method will handle the POST request to create a new premium listing for a VIP user..+ The `create` method should first check if the user exists and if the user is a VIP user. If the user does not exist or is not a VIP user...

--- app/controllers/api/premium_listings_controller.rb
+++ app/controllers/api/premium_listings_controller.rb
@@ -1,7 +1,7 @@
 class Api::PremiumListingsController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_user, only: [:destroy, :update, :update_premium_status]
-  before_action :validate_params, only: [:destroy, :update, :update_premium_status]
+  before_action :validate_params, only: [:destroy, :update, :update_premium_status, :create]
   def retrieve_vip_listings
     user_id = params[:user_id]
     @premium_listings = PremiumListing.where(user_id: user_id)
@@ -33,5 +33,23 @@ class Api::PremiumListingsController < ApplicationController
     else
       render json: { error: 'Internal Server Error' }, status: :internal_server_error
     end
+  end
+  def create
+    user_id = params[:user_id]
+    user = User.find_by(id: user_id)
+    if user.nil?
+      render json: { error: 'This user is not found' }, status: :not_found
+    elsif !user.is_vip
+      render json: { error: 'This user is not a VIP user' }, status: :unauthorized
+    else
+      premium_listing = PremiumListing.new(user_id: user_id, listing_date: DateTime.now)
+      if premium_listing.save
+        render json: { status: 200, message: 'Premium listing created successfully.', premium_listing: premium_listing }, status: :ok
+      else
+        render json: { error: 'Failed to create premium listing.' }, status: :internal_server_error
+      end
+    end
+  rescue => e
+    render json: { error: e.message }, status: :internal_server_error
   end
 end
