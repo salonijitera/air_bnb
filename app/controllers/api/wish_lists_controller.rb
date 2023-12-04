f31faf1cce486385f0a6cf0e8c887bbfe15c4758
Danette (Jitera) <developers@jitera.com>
2023-12-04 11:16:08 +0000

    feat[api]: implement 'Share Wish List' Changes * '/app/controllers/api/wish_lists_controller.rb': + Add a new route in the routes file for the share wish list API. The route should be a POST request to '/api/wish_lists/{id}/share'..+ In the WishListsController, add a new action 'share' which will handle the request for sharing the wish list..+ In the 'share' action, first, authenticate the user ...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -2,13 +2,56 @@ class Api::WishListsController < ApplicationController
   before_action :authenticate_user!, only: [:share, :create, :share_wish_list, :update_wish_list, :update]
   before_action :authorize_user!, only: [:create, :update_wish_list, :update]
   def create
-    # ... existing code ...
+    validator = WishListValidator.new(wish_list_params)
+    unless validator.valid?
+      render json: { errors: validator.errors.full_messages }, status: :bad_request
+      return
+    end
+    begin
+      wish_list = WishListService.create_wish_list(wish_list_params)
+      render json: { status: 200, wish_list: wish_list }, status: :ok
+    rescue => e
+      render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
+    end
   end
-  def share_wish_list
-    # ... existing code ...
+  def share
+    wish_list_id = params[:id]
+    email = params[:email]
+    unless wish_list_id.to_i.to_s == wish_list_id && email =~ URI::MailTo::EMAIL_REGEXP
+      render json: { message: 'Wrong format or Invalid email address' }, status: :bad_request
+      return
+    end
+    wish_list = WishList.find_by(id: wish_list_id)
+    user = User.find_by(email: email)
+    unless wish_list && user
+      render json: { message: 'This wish list is not found' }, status: :not_found
+      return
+    end
+    begin
+      WishListService.share(wish_list, user)
+      render json: { status: 200, message: 'Wish list was successfully shared.' }, status: :ok
+    rescue => e
+      render json: { message: 'Failed to share wish list', error: e.message }, status: :internal_server_error
+    end
   end
   def update_wish_list
-    # ... existing code ...
+    wish_list_id = params[:wish_list_id]
+    property_id = params[:property_id]
+    unless WishList.exists?(wish_list_id) && Property.exists?(property_id)
+      render json: { message: 'Invalid wish list or property id' }, status: :bad_request
+      return
+    end
+    begin
+      if WishListService.property_in_wish_list?(wish_list_id, property_id)
+        WishListService.remove_property(wish_list_id, property_id)
+      else
+        WishListService.add_property(wish_list_id, property_id)
+      end
+      updated_wish_list = WishListService.get_wish_list(wish_list_id)
+      render json: { message: 'Wish list updated successfully', wish_list: updated_wish_list }, status: :ok
+    rescue => e
+      render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
+    end
   end
   def update
     wish_list_id = params[:id]
