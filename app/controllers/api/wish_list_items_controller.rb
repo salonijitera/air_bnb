bdc569a7fb534fd2c3bfd060b937d6dd79ecbf46
Jenny (Jitera) <developers@jitera.com>
2023-12-04 07:08:24 +0000

    feat[api]: implement 'Add property to wish list' Changes * '/app/controllers/api/wish_list_items_controller.rb': + In the `Api::WishListItemsController` class, add a `create` method. This method should take in `property_id` and `wish_list_id` as parameters from the request body..+ Use the `WishListService` to check if the property and wish list exist. If they do not exist, return a 400 Bad Request with an erro...

--- app/controllers/api/wish_list_items_controller.rb
+++ app/controllers/api/wish_list_items_controller.rb
@@ -1,14 +1,22 @@
 class Api::WishListItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_user!
   def create
     begin
       property_id = params[:property_id]
       wish_list_id = params[:wish_list_id]
+      # Validate the property_id and wish_list_id
+      validator = WishListItemValidator.new(property_id: property_id, wish_list_id: wish_list_id)
+      unless validator.valid?
+        render json: { error: 'Wrong format' }, status: :unprocessable_entity
+        return
+      end
       if Property.exists?(property_id) && WishList.exists?(wish_list_id)
         wish_list_item = WishListItem.new(property_id: property_id, wish_list_id: wish_list_id)
         if wish_list_item.save
-          render json: { message: 'Wish list item added successfully', wish_list_item: wish_list_item }, status: :ok
+          render json: { status: 200, wish_list_item: wish_list_item }, status: :ok
         else
-          render json: { error: 'Failed to add item to wish list' }, status: :unprocessable_entity
+          render json: { error: 'Failed to add item to wish_list' }, status: :unprocessable_entity
         end
       else
         render json: { error: 'Invalid property or wish list' }, status: :bad_request
@@ -16,5 +24,12 @@ class Api::WishListItemsController < ApplicationController
     rescue => e
       render json: { error: e.message }, status: :internal_server_error
     end
+  end
+  private
+  def authenticate_user!
+    # Implement authentication logic here
+  end
+  def authorize_user!
+    # Implement authorization logic here
   end
 end
