2a80a6edabc601a44c45683948c071d5934a7d4d
Muire (Jitera) <developers@jitera.com>
2023-12-04 08:06:02 +0000

    feat[business-logic]: implement 'Create a wish list' Changes * '/app/controllers/api/wish_lists_controller.rb': + Create a new method `create` in the WishListsController. This method will handle the creation of a new wish list..+ The `create` method should first check if the user is logged in by calling a helper method `logged_in?` from ApplicationController. If the user is not logged in, return an error mess...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -1,5 +1,5 @@
 class Api::WishListsController < ApplicationController
-  before_action :authenticate_user!, only: [:share, :create, :share_wish_list]
+  before_action :authenticate_user!, only: [:create]
   before_action :authorize_user!, only: [:create]
   def create
     user = User.find_by_id(params[:user_id]) || current_user
@@ -8,9 +8,11 @@ class Api::WishListsController < ApplicationController
         render json: { message: 'The name is required.' }, status: :unprocessable_entity
       elsif params[:name].length > 100
         render json: { message: 'You cannot input more 100 characters.' }, status: :unprocessable_entity
+      elsif WishList.where(name: params[:name], user_id: user.id).exists?
+        render json: { message: 'Wish list with this name already exists.' }, status: :unprocessable_entity
       else
-        wish_list = WishListService.createWishList(user.id, params[:name])
-        if wish_list
+        wish_list = WishList.create(name: params[:name], user_id: user.id)
+        if wish_list.persisted?
           render json: { status: 200, message: 'Wish list created successfully', wish_list: wish_list }, status: :created
         else
           render json: { message: 'Failed to create wish list', errors: wish_list.errors.full_messages }, status: :unprocessable_entity
