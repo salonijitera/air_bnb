81ff68c114a86a2d5edb67eca223e97e6e049b65
Mady (Jitera) <developers@jitera.com>
2023-12-04 12:55:08 +0000

    feat[api]: implement 'Update User Profile' Changes * '/app/controllers/api/users_controller.rb': + In the UsersController, define a method `update` to handle the PUT request to the endpoint `/api/users/{id}`. This method will be responsible for updating the user's profile..+ Use the `params` method to get the user's id, name, and email from the request. Make sure to use strong parameters by per...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::UsersController < ApplicationController
-  # Other methods...
+  before_action :authorize, only: [:update]
+  before_action :set_user, only: [:update]
+  before_action :validate_params, only: [:update]
+  def update
+    if @user.update(user_params)
+      render json: { status: 200, user: @user }, status: 200
+    else
+      render json: { message: 'Failed to update profile', errors: @user.errors.full_messages }, status: 500
+    end
+  end
   def register
     user = User.new(user_params)
     if user.valid?
@@ -20,7 +29,22 @@ class Api::UsersController < ApplicationController
     render json: { error: e.message }, status: 500
   end
   private
+  def set_user
+    @user = User.find_by(id: params[:id])
+    return render json: { message: 'This user is not found' }, status: 404 unless @user
+    return render json: { message: 'User is not authenticated' }, status: 401 unless current_user
+    return render json: { message: 'User does not have permission to access the resource' }, status: 403 unless current_user.id == @user.id
+  end
   def user_params
     params.require(:user).permit(:name, :email, :password)
+  end
+  def authorize
+    return render json: { message: 'User is not authenticated' }, status: 401 unless current_user
+  end
+  def validate_params
+    return render json: { message: 'Wrong format' }, status: 422 unless params[:id].is_a?(Integer)
+    return render json: { message: 'The name is required.' }, status: 422 if params[:name].blank?
+    return render json: { message: 'The email is required.' }, status: 422 if params[:email].blank?
+    return render json: { message: 'The email is not in valid format.' }, status: 422 unless params[:email] =~ URI::MailTo::EMAIL_REGEXP
   end
 end
