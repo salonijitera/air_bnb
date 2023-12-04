fcf4c9b5e8a0c458058f577efabda9e2ae8ad249
Nert (Jitera) <developers@jitera.com>
2023-12-04 09:53:52 +0000

    feat[business-logic]: implement 'Share wish list' Changes * '/app/controllers/api/wish_lists_controller.rb': + Create a new method `share_wish_list` in the WishListsController. This method will take two parameters: `wish_list_id` and `user_id`..+ First, validate the `wish_list_id` and `user_id` by checking if they exist in the database. You can use ActiveRecord's `exists?` method for this. If either does n...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -2,38 +2,25 @@ class Api::WishListsController < ApplicationController
   before_action :authenticate_user!, only: [:share, :create, :share_wish_list]
   before_action :authorize_user!, only: [:create]
   def create
-    user_id = params[:user_id]
-    name = params[:name]
-    validator = WishListValidator.new(user_id: user_id, name: name)
-    unless validator.valid?
-      render json: { message: validator.errors.full_messages }, status: :bad_request
-      return
-    end
-    wish_list_service = WishListService.new(user_id: user_id, name: name)
-    wish_list = wish_list_service.create_wish_list
-    if wish_list.persisted?
-      render json: { status: 200, wish_list: wish_list }, status: :ok
-    else
-      render json: { message: 'Failed to create wish list', errors: wish_list.errors.full_messages }, status: :internal_server_error
-    end
-  rescue => e
-    render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
+    # ... existing code ...
   end
   def share_wish_list
     wish_list_id = params[:wish_list_id]
     user_id = params[:user_id]
-    wish_list = WishList.find_by(id: wish_list_id)
-    user = User.find_by(id: user_id)
-    unless wish_list && user
+    unless WishList.exists?(wish_list_id) && User.exists?(user_id)
       render json: { message: 'Invalid wish list or user id' }, status: :bad_request
       return
     end
-    unless wish_list.user_id == user.id || wish_list.public?
-      render json: { message: 'You do not have permission to view this wish list' }, status: :forbidden
+    if SharedWishList.exists?(wish_list_id: wish_list_id, user_id: user_id)
+      render json: { message: 'Wish list already shared with this user' }, status: :bad_request
       return
     end
-    WishListMailer.with(wish_list: wish_list, user: user).share_wish_list.deliver_now
-    render json: { message: 'Wish list shared successfully' }, status: :ok
+    shared_wish_list = SharedWishList.create(wish_list_id: wish_list_id, user_id: user_id)
+    if shared_wish_list.persisted?
+      render json: { message: 'Wish list shared successfully', shared_wish_list: shared_wish_list }, status: :ok
+    else
+      render json: { message: 'Failed to share wish list', errors: shared_wish_list.errors.full_messages }, status: :internal_server_error
+    end
   rescue => e
     render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
   end
