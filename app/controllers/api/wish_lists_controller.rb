badf264ebd4d913cc3034cec04dfe7fbd2ce58c6
Luz (Jitera) <developers@jitera.com>
2023-12-04 09:59:42 +0000

    feat[api]: implement 'Create a Wish List' Changes * '/app/views/api/wish_lists/create.json.jbuilder': + In the create.json.jbuilder view, display the details of the created wish list. You can use the _wish_list.json.jbuilder partial for this purpose..+ The details should include the id, user_id, and name of the wish list.... * '/app/controllers/api/wish_lists_controller.rb': + In the WishListsController, define a create method that handles the POST request to the /api/wish_lists endpoint..+ This method should first authenticate the user by calling a method from ApplicationController (e.g., authenticate_user!)..+ Then, it should validate the request parameters. You can u...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -2,7 +2,17 @@ class Api::WishListsController < ApplicationController
   before_action :authenticate_user!, only: [:share, :create, :share_wish_list]
   before_action :authorize_user!, only: [:create]
   def create
-    # ... existing code ...
+    validator = WishListValidator.new(wish_list_params)
+    unless validator.valid?
+      render json: { errors: validator.errors.full_messages }, status: :bad_request
+      return
+    end
+    begin
+      wish_list = WishListService.create_wish_list(wish_list_params)
+      render json: { status: 200, wish_list: wish_list }, status: :ok
+    rescue => e
+      render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
+    end
   end
   def share_wish_list
     wish_list_id = params[:wish_list_id]
@@ -26,6 +36,10 @@ class Api::WishListsController < ApplicationController
   end
   def share
     # ... existing code ...
+  end
+  private
+  def wish_list_params
+    params.require(:wish_list).permit(:name, :user_id)
   end
   # ... rest of the code ...
 end
--- app/views/api/wish_lists/create.json.jbuilder
+++ app/views/api/wish_lists/create.json.jbuilder
@@ -1,6 +1,6 @@
 json.status 200
 json.wish_list do
   json.id @wish_list.id
-  json.name @wish_list.name
   json.user_id @wish_list.user_id
+  json.name @wish_list.name
 end
