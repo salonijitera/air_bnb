a76e6218d4e42e4777c6267f670fc88e512ffc15
Dolley (Jitera) <developers@jitera.com>
2023-12-04 12:43:48 +0000

    feat[api]: implement 'Share Wish List' Changes * '/app/controllers/api/wish_lists_controller.rb': + Add a new method `share` in the `WishListsController` to handle the sharing of wish lists. This method should be a POST request and should be authenticated..+ The `share` method should first validate the input parameters. If the `id` is not a number, return a 422 status code with the error message...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -1,6 +1,6 @@
 class Api::WishListsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_wish_list, only: [:share_wish_list]
+  before_action :find_wish_list, only: [:share_wish_list, :share]
   def create
     return render json: { error: 'Unauthorized' }, status: 401 unless user_signed_in?
     return render json: { error: 'Wrong format' }, status: 400 unless params[:user_id].is_a?(Integer)
@@ -14,10 +14,56 @@ class Api::WishListsController < ApplicationController
       Notification.create(user_id: params[:user_id], message: 'Wish list created', status: 'unread')
       render json: { status: 200, wish_list: @wish_list }, status: 200
     else
-      render json: { error: 'Failed to create wish_list' }, status: 500
+      render json: { error: 'Failed to create wish list' }, status: 500
     end
   rescue => e
     render json: { error: e.message }, status: 500
   end
-  # ... rest of the code
+  def add_property
+    wish_list_id = params[:wish_list_id]
+    property_id = params[:property_id]
+    # Validate wish_list_id and property_id
+    unless WishList.exists?(wish_list_id) && Property.exists?(property_id)
+      return render json: { error: 'Invalid wish list or property id' }, status: 404
+    end
+    # Check if the property is already in the wish list
+    if WishListItem.exists?(wish_list_id: wish_list_id, property_id: property_id)
+      return render json: { error: 'Property is already in the wish list' }, status: 400
+    end
+    # Add the property to the wish list
+    wish_list_item = WishListItem.new(wish_list_id: wish_list_id, property_id: property_id)
+    if wish_list_item.save
+      # Send confirmation to the user
+      render json: { status: 200, message: 'Property successfully added to the wish list', wish_list: WishList.find(wish_list_id).as_json(include: :properties) }
+    else
+      render json: { error: 'Failed to add property to the wish list' }, status: 500
+    end
+  rescue => e
+    render json: { error: e.message }, status: 500
+  end
+  def share
+    begin
+      return render json: { error: 'Wrong format' }, status: 422 unless params[:id].is_a?(Integer)
+      return render json: { error: 'Invalid email format' }, status: 422 unless params[:email] =~ URI::MailTo::EMAIL_REGEXP
+      user = User.find_by(email: params[:email])
+      if @wish_list.users.include?(user)
+        render json: { error: "User is already a collaborator" }, status: 422
+      else
+        @wish_list.users << user
+        @wish_list.save
+        NotificationService.new(user, "You have been added as a collaborator to the wish list: #{@wish_list.name}").send
+        render json: { status: 200, message: 'Wish list was successfully shared.' }
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "This wish list is not found" }, status: 404
+    rescue => e
+      render json: { error: e.message }, status: 500
+    end
+  end
+  private
+  def find_wish_list
+    @wish_list = WishList.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "This wish list is not found" }, status: 404
+  end
 end
