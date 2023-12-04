b074d71b6735cec0f17e6c836c29b1ba1f315665
Daffi (Jitera) <developers@jitera.com>
2023-12-04 09:22:42 +0000

    feat[business-logic]: implement 'Add property to wish list' Changes * '/app/controllers/api/wish_list_items_controller.rb': + Create a new method `add_property` in the `WishListItemsController`. This method will handle the logic of adding a property to a wish list..+ The `add_property` method should take two parameters: `wish_list_id` and `property_id`..+ First, validate the `wish_list_id` and `property_id` by checking i...

--- app/controllers/api/wish_list_items_controller.rb
+++ app/controllers/api/wish_list_items_controller.rb
@@ -4,19 +4,30 @@ class Api::WishListItemsController < ApplicationController
   def add_property
     wish_list_id = params[:wish_list_id]
     property_id = params[:property_id]
-    wish_list = WishList.find_by(id: wish_list_id)
-    property = Property.find_by(id: property_id)
-    unless wish_list && property
-      render json: { error: 'Wish list or property not found' }, status: :not_found
+    # Validate parameters
+    unless wish_list_id && property_id && wish_list_id.is_a?(Integer) && property_id.is_a?(Integer)
+      render json: { error: 'Wrong format' }, status: :bad_request
       return
     end
+    wish_list = WishList.find_by(id: wish_list_id)
+    property = Property.find_by(id: property_id)
+    # Check if wish list and property exist
+    if wish_list.nil?
+      render json: { error: 'This wish_list is not found' }, status: :not_found
+      return
+    elsif property.nil?
+      render json: { error: 'This property is not found' }, status: :not_found
+      return
+    end
+    # Check if property is already in wish list
     if WishListItem.exists?(wish_list_id: wish_list_id, property_id: property_id)
       render json: { error: 'Property already in wish list' }, status: :unprocessable_entity
       return
     end
+    # Add property to wish list
     wish_list_item = WishListItem.create(wish_list_id: wish_list_id, property_id: property_id)
     if wish_list_item.persisted?
-      render json: { status: 200, message: 'Property successfully added to wish list', wish_list: wish_list }, status: :ok
+      render json: { status: 200, wish_list_item: wish_list_item }, status: :ok
     else
       render json: { error: 'Failed to add property to wish_list' }, status: :internal_server_error
     end
