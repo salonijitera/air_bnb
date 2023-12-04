bd66461debe48f630cfb2d5a64d0513c5c8b015d
Harriett (Jitera) <developers@jitera.com>
2023-12-04 08:12:29 +0000

    feat[api]: implement 'Create a Wish List' Changes * '/app/controllers/api/wish_lists_controller.rb': + In the `Api::WishListsController` class, write a `create` method that takes in parameters "user_id" and "name"..+ Use the `find_by_id` method to check if the user exists in the database. If the user does not exist, return a 404 error with the message "This user is not found"..+ Validate the "user_...

--- app/controllers/api/wish_lists_controller.rb
+++ app/controllers/api/wish_lists_controller.rb
@@ -1,49 +1,39 @@
 class Api::WishListsController < ApplicationController
-  before_action :authenticate_user!, only: [:share, :create, :share_wish_list]
+  before_action :authenticate_user!, only: [:create]
   before_action :authorize_user!, only: [:create]
   def create
-    user = User.find_by_id(params[:user_id]) || current_user
-    if user
-      if params[:name].blank?
-        render json: { message: 'The name is required.' }, status: :unprocessable_entity
-      elsif params[:name].length > 100
-        render json: { message: 'You cannot input more 100 characters.' }, status: :unprocessable_entity
-      elsif WishList.where(name: params[:name], user_id: user.id).exists?
-        render json: { message: 'Wish list with this name already exists.' }, status: :unprocessable_entity
+    user_id = params[:user_id]
+    name = params[:name]
+    # Validate user_id
+    if user_id.blank? || !(user_id.is_a? Integer)
+      render json: { message: 'Wrong format.' }, status: :bad_request
+      return
+    end
+    # Find user
+    user = User.find_by_id(user_id) || current_user
+    if user.nil?
+      render json: { message: 'This user is not found.' }, status: :not_found
+      return
+    end
+    # Validate name
+    if name.blank?
+      render json: { message: 'The name is required.' }, status: :bad_request
+      return
+    elsif name.length > 100
+      render json: { message: 'You cannot input more than 100 characters.' }, status: :bad_request
+      return
+    end
+    # Create wish list
+    begin
+      wish_list = WishListService.createWishList(user.id, name)
+      if wish_list
+        render json: { status: 200, message: 'Wish list created successfully', wish_list: wish_list }, status: :created
       else
-        wish_list = WishListService.createWishList(user.id, params[:name])
-        if wish_list
-          render json: { status: 200, message: 'Wish list created successfully', wish_list: wish_list }, status: :created
-        else
-          render json: { message: 'Failed to create wish list', errors: wish_list.errors.full_messages }, status: :unprocessable_entity
-        end
+        render json: { message: 'Failed to create wish list', errors: wish_list.errors.full_messages }, status: :internal_server_error
       end
-    else
-      render json: { message: 'User not found' }, status: :not_found
+    rescue => e
+      render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
     end
-  rescue => e
-    render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
-  end
-  def share_wish_list
-    wish_list = WishList.find_by_id(params[:wish_list_id])
-    user = User.find_by_id(params[:user_id])
-    if wish_list.nil? || user.nil?
-      render json: { message: 'Wish list or User not found' }, status: :not_found
-      return
-    end
-    if wish_list.user_id != current_user.id
-      render json: { message: 'You do not have permission to share this wish list' }, status: :forbidden
-      return
-    end
-    shared_wish_list = SharedWishList.create(wish_list_id: wish_list.id, user_id: user.id)
-    if shared_wish_list.persisted?
-      ApplicationMailer.share_wish_list_email(user, wish_list).deliver_now
-      render json: { message: 'Wish list shared successfully' }, status: :ok
-    else
-      render json: { message: 'Failed to share wish list', errors: shared_wish_list.errors.full_messages }, status: :unprocessable_entity
-    end
-  rescue => e
-    render json: { message: 'Internal Server Error', error: e.message }, status: :internal_server_error
   end
   # ... rest of the code
 end
