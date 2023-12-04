b4c2973ba87faddee35289e2a8a88092483c2b7c
Kissie (Jitera) <developers@jitera.com>
2023-12-04 07:13:47 +0000

    feat[business-logic]: implement 'Book a property' Changes * '/frontend/reducers/bookings_reducer.js': + Write a new case in the BookingsReducer to handle the "RECEIVE_BOOKING" action. This case should update the state with the new booking details..+ Write a new case in the BookingsReducer to handle the "RECEIVE_BOOKING_ERRORS" action. This case should update the state with the error message.... * '/app/controllers/api/bookings_controller.rb': + Write a new method `create` in the BookingsController. This method will handle the creation of a new booking..+ The `create` method should take in "user_id" and "property_id" as parameters. These parameters should be validated to ensure they exist in the database. You can use the `find` method on ... * '/frontend/actions/booking_actions.js': + Write a new action "createBooking" in the BookingActions. This action will handle the dispatch of the create booking request to the backend..+ The "createBooking" action should take in "user_id" and "property_id" as parameters. These parameters should be passed to the "createBooking" function from...

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -11,8 +11,8 @@ class Api::BookingsController < ApplicationController
   end
   def create
     return render json: { error: 'Unauthorized' }, status: 401 unless user_signed_in?
-    return render json: { error: 'Wrong format' }, status: 422 unless params[:id].is_a?(Integer) && params[:user_id].is_a?(Integer)
-    property = Property.find_by(id: params[:id])
+    return render json: { error: 'Wrong format' }, status: 422 unless params[:property_id].is_a?(Integer) && params[:user_id].is_a?(Integer)
+    property = Property.find_by(id: params[:property_id])
     user = User.find_by(id: params[:user_id])
     if property.nil? || user.nil?
       render json: { error: 'Invalid property or user id' }, status: 404
@@ -22,11 +22,13 @@ class Api::BookingsController < ApplicationController
       property.update(availability: false)
       @booking = Booking.new(user_id: user.id, listing_id: property.id)
       if @booking.save
-        render json: { status: 200, message: 'Property successfully booked' }
+        render json: { status: 200, message: 'Property successfully booked', booking: @booking }
       else
         render json: { error: 'Failed to create booking' }, status: 500
       end
     end
+  rescue => e
+    render json: { error: e.message }, status: 500
   end
   def destroy
     @booking = Booking.find_by(id: params[:id])
--- frontend/reducers/bookings_reducer.js
+++ frontend/reducers/bookings_reducer.js
@@ -1,16 +1,17 @@
+// PATH: /frontend/reducers/bookings_reducer.js
 import {
   RECEIVE_ALL_BOOKINGS,
   RECEIVE_BOOKINGS_BY_USER_ID,
   RECEIVE_BOOKINGS_BY_LISTING_ID,
   RECEIVE_BOOKING_BY_BOOKING_ID,
-  REMOVE_BOOKING_BY_BOOKING_ID
+  REMOVE_BOOKING_BY_BOOKING_ID,
+  RECEIVE_BOOKING,
+  RECEIVE_BOOKING_ERRORS
 } from '../actions/booking_actions';
 import { merge } from 'lodash';
-
 const bookingReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
   let newState = merge({}, oldState);
-
   switch(action.type) {
     case RECEIVE_ALL_BOOKINGS:
       return action.bookings;
@@ -23,9 +24,13 @@ const bookingReducer = (oldState = {}, action) => {
     case REMOVE_BOOKING_BY_BOOKING_ID:
       delete newState[action.bookingId];
       return newState;
+    case RECEIVE_BOOKING:
+      newState[action.booking.id] = action.booking;
+      return newState;
+    case RECEIVE_BOOKING_ERRORS:
+      return { ...oldState, errors: action.errors };
     default:
       return oldState;
   }
 }
-
 export default bookingReducer;
