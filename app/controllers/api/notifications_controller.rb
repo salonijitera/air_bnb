f26d914ac162d3ab0ac5817f12e754f4707c77d0
Lindsy (Jitera) <developers@jitera.com>
2023-12-04 07:51:21 +0000

    feat[business-logic]: implement 'Receive notifications' Changes * '/app/controllers/api/notifications_controller.rb': + Create a new method called 'index' in the NotificationsController. This method will handle the retrieval of notifications for a user..+ The 'index' method should take 'user_id' as a parameter. Use this 'user_id' to validate the user's session and ensure the user has an active account. You can use ...

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -1,9 +1,22 @@
 class Api::NotificationsController < ApplicationController
-  before_action :authenticate_user, only: [:get_notifications]
-  before_action :authorize_user, only: [:get_notifications]
+  before_action :authenticate_user, only: [:get_notifications, :index]
+  before_action :authorize_user, only: [:get_notifications, :index]
   def get_notifications
     begin
-      user_id = params[:user_id]
+      user_id = params[:user_id].to_i
+      if user_id.nil? || !user_id.is_a?(Integer)
+        render json: { error: 'Wrong format' }, status: :bad_request
+      else
+        notifications = Notification.where(user_id: user_id).select(:id, :message, :status, :created_at)
+        render json: { status: 200, notifications: notifications }, status: :ok
+      end
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+  def index
+    begin
+      user_id = params[:user_id].to_i
       if user_id.nil? || !user_id.is_a?(Integer)
         render json: { error: 'Wrong format' }, status: :bad_request
       else
