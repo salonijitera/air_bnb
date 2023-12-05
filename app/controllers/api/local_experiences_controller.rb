46712854dc2dafce4793ebcaf9ea18509f1a35c5
Danyelle (Jitera) <developers@jitera.com>
2023-12-05 05:55:01 +0000

    feat[business-logic]: implement 'Delete local experience' Changes * '/app/controllers/api/local_experiences_controller.rb': + Write a new method `delete_local_experience` in the `LocalExperiencesController`. This method will take "id" as a parameter..+ Use the `LocalExperienceService` to check if the "id" exists in the "LocalExperience" table. If it does not exist, return an error message..+ If the "id" exists, use the `...

--- app/controllers/api/local_experiences_controller.rb
+++ app/controllers/api/local_experiences_controller.rb
@@ -1,43 +1,7 @@
 class Api::LocalExperiencesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :destroy, :update]
-  before_action :authorize_user!, only: [:destroy]
-  def create
-    validator = LocalExperienceValidator.new(local_experience_params)
-    unless validator.valid?
-      render json: { error: validator.errors.full_messages }, status: 422
-      return
-    end
-    location = Location.find_by(name: local_experience_params[:location])
-    unless location
-      render json: { error: "Location not found!" }, status: 422
-      return
-    end
-    begin
-      local_experience = LocalExperience.create!(local_experience_params.merge(location_id: location.id))
-      render json: { status: 200, message: "Local experience was successfully created.", localExperience: local_experience }, status: 200
-    rescue => e
-      render json: { error: e.message }, status: 500
-    end
-  end
-  def update
-    validator = LocalExperienceValidator.new(local_experience_params)
-    unless validator.valid?
-      render json: { error: validator.errors.full_messages }, status: 422
-      return
-    end
-    local_experience = LocalExperience.find_by(id: params[:id])
-    unless local_experience
-      render json: { error: "This local experience is not found." }, status: 404
-      return
-    end
-    begin
-      local_experience.update!(local_experience_params)
-      render json: { status: 200, message: "Local experience was successfully updated.", localExperience: local_experience }, status: 200
-    rescue => e
-      render json: { error: e.message }, status: 500
-    end
-  end
-  def destroy
+  before_action :authenticate_user!, only: [:create, :destroy, :update, :delete_local_experience]
+  before_action :authorize_user!, only: [:destroy, :delete_local_experience]
+  def delete_local_experience
     id = params[:id].to_i
     unless id > 0
       render json: { error: "Wrong format." }, status: 422
@@ -49,18 +13,9 @@ class Api::LocalExperiencesController < ApplicationController
       return
     end
     begin
+      local_experience.image.purge
       local_experience.destroy
       render json: { status: 200, message: "The local experience was successfully deleted." }, status: 200
-    rescue => e
-      render json: { error: e.message }, status: 500
-    end
-  end
-  def show
-    begin
-      local_experience = LocalExperience.find(params[:id])
-      render json: local_experience, status: 200
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: "Local experience not found!" }, status: 404
     rescue => e
       render json: { error: e.message }, status: 500
     end
